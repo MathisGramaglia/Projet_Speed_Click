ae666c5a86339d8fe2a88f057d273fa7da49eb33
mathi <mathi@HP-Mathis.lan>
2023-12-22 17:04:47 +0100

    Classe CelluleGraphique opérationnelle :

--- Projet_Speed_Click_Graphique/src/CelluleGraphique.java
+++ Projet_Speed_Click_Graphique/src/CelluleGraphique.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import projet_speed_click.Bouton;
 
@@ -11,37 +13,24 @@ import projet_speed_click.Bouton;
  * Étend JButton pour permettre l'interaction utilisateur.
  */
 public class CelluleGraphique extends JButton {
-    private final int largeur;  // Largeur en pixels de la cellule
-    private final int hauteur;  // Hauteur en pixels de la cellule
-    private final Bouton boutonAssocie;  // Bouton associé à la cellule
+    private final int ligne;
+    private final int colonne;
 
-    /**
-     * Constructeur de la classe CelluleGraphique.
-     *
-     * @param boutonAssociee Bouton associé à la cellule.
-     * @param l              Largeur en pixels de la cellule.
-     * @param h              Hauteur en pixels de la cellule.
-     */
-    public CelluleGraphique(Bouton boutonAssocie, int l, int h) {
-        this.boutonAssocie = boutonAssocie;
-        this.largeur = l;
-        this.hauteur = h;
+    public CelluleGraphique(Bouton boutonAssocie, int l, int h, int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
 
-        // Configure le texte du bouton lors de la création
         this.setText(boutonAssocie.toString());
-    }
-
-    /**
-     * Méthode gérant le dessin de la cellule.
-     *
-     * @param g Graphics utilisé pour le dessin.
-     */
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        // Aucun besoin de configurer le texte ici car cela a été fait dans le constructeur.
-        // Si nécessaire, d'autres opérations de dessin peuvent être ajoutées ici.
+        
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Ajoutez ici le code que vous souhaitez exécuter lorsque le bouton est cliqué
+                System.out.println("Bouton à la ligne " + ligne + ", colonne " + colonne + " cliqué !");
+            }
+        });
     }
 }
+
 
 
