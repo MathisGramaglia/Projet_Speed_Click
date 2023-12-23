6c75d555ca1de6005681a1e0d67c31a66508dfca
mathi <mathi@HP-Mathis.lan>
2023-12-23 21:20:02 +0100

    Classe DarthVaderButton: Cette classe affiche un bouton graphique  contenant l'image de Dark Vador. Cela permet au joueur de démarrer  une version autonome du bouton Star Wars.

--- SpeedClick_Star_Wars/src/speedclick_star_wars/SpeedClick_Star_Wars.java
+++ SpeedClick_Star_Wars/src/speedclick_star_wars/SpeedClick_Star_Wars.java
@@ -4,16 +4,22 @@
  */
 package speedclick_star_wars;
 
+/**
+ * Classe principale pour le jeu SpeedClick Star Wars.
+ * Cette classe contient le point d'entrée de l'application et initialise la fenêtre d'accueil (WelcomeWindow).
+ * 
+ * @author Mathi
+ * @version 1.0
+ */
+
 import javax.swing.SwingUtilities;
 
-/**
- *
- * @author mathi
- */
 public class SpeedClick_Star_Wars {
 
     /**
-     * @param args the command line arguments
+     * Méthode principale de l'application, qui initialise la fenêtre d'accueil (WelcomeWindow) et la rend visible.
+     * 
+     * @param args les arguments de ligne de commande (non utilisés)
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
@@ -22,3 +28,4 @@ public class SpeedClick_Star_Wars {
         });
     }
 }
+
