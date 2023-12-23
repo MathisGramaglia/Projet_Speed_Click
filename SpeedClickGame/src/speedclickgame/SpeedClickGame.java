68f8476c697f2fc2ee701d5e0a7056eb5ff1da7b
mathi <mathi@HP-Mathis.lan>
2023-12-23 18:38:34 +0100

    Main du projet

---
+++ SpeedClickGame/src/speedclickgame/SpeedClickGame.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package speedclickgame;
+
+/**
+ *
+ * @author mathi
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class SpeedClickGame extends JFrame {
+
+    public SpeedClickGame() {
+    }
+}
